f54750d2cfe627b863ac824f34c651030da85eca
Katarzyna <katarzyna.niziol1993@gmail.com>
2022-04-19 11:58:14 +0200

    Initial commit

---
+++ src/isSkyBlue/Main2.java
@@ -0,0 +1,13 @@
+package isSkyBlue;
+
+public class Main2 {
+    public static void main(String[] args) {
+         boolean isSkyBlue = true;
+
+         if (true){
+             System.out.println("Niebo jest niebieske");
+         }else {
+             System.out.println("Niebo nie jest niebieskie");
+         }
+    }
+}
---
+++ src/isSkyBlue/PetlaFor.java
@@ -0,0 +1,16 @@
+package isSkyBlue;
+
+public class PetlaFor {
+    public static void main(String[] args) {
+      for (int i = 0, j= 10; i<=10; i++, j-- ) {
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println( );
+        }
+
+        int k = 0;
+        for (; k < 10; k++) {
+            System.out.println(k);
+        }
+    }
+}
